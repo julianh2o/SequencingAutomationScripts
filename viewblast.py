fe1371dcc55bc79dd07a884e39fc59a919753258
Julian Hartline <julianh2o@gmail.com>
2015-02-05 14:39:38 -0800

    added the ability to retrieve the accession number with viewblast info command

--- viewblast.py
+++ viewblast.py
@@ -12,12 +12,17 @@ import xml.etree.ElementTree as ET
 parser = argparse.ArgumentParser(description='View the XML blast results')
 command_subparser = parser.add_subparsers(help="View mode")
 
+info_parser = command_subparser.add_parser("info",help="View a single result");
+info_parser.add_argument("index", type=int);
+info_parser.add_argument("-f","--format", help="Format of the response", default="{accession}");
+info_parser.add_argument('blast', type=argparse.FileType('r'), nargs="?", default=sys.stdin, help='the BLAST results file in XML')
+
 contig_parser = command_subparser.add_parser("contig",help="View a result's contig");
 contig_parser.add_argument("index", type=int);
 contig_parser.add_argument('blast', type=argparse.FileType('r'), nargs="?", default=sys.stdin, help='the BLAST results file in XML')
 
 list_parser = command_subparser.add_parser("list",help="List the results");
-list_parser.add_argument("-n", "--max", type=int, help="Number of records to show");
+list_parser.add_argument("-n","--max", type=int, help="Number of records to show");
 list_parser.add_argument('blast', type=argparse.FileType('r'), nargs="?", default=sys.stdin, help='the BLAST results file in XML')
 
 
@@ -42,6 +47,24 @@ def formatTable(output):
             i += 1;
         print();
             
+def info(args):
+    results = args.blast.read();
+
+    rootNode = ET.fromstring(results);
+    alignments = rootNode.findall("BlastOutput_iterations/Iteration/Iteration_hits/Hit")
+    i = 1;
+    for alignment in alignments:
+        hitid = alignment.find("Hit_id").text;
+        hitdef = alignment.find("Hit_def").text;
+        accession = alignment.find("Hit_accession").text;
+        evalue = float(alignment.find("Hit_hsps/Hsp/Hsp_evalue").text);
+        if (i == args.index):
+            fstring = args.format;
+            fstring = fstring.replace("{accession}",accession);
+            print(fstring);
+            exit(0);
+        i += 1
+info_parser.set_defaults(func=info);
 
 def contig(args):
     results = args.blast.read();
