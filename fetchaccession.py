f65ad7eaeecc695d0c306621891cb22f4e9ccb6d
Julian Hartline <julianh2o@gmail.com>
2015-09-30 22:06:41 -1000

    improved fetch accesion to accept arguments on stdin

--- fetchaccession.py
+++ fetchaccession.py
@@ -2,6 +2,20 @@
 import requests
 import sys
 import time
+import argparse
+
+#http://stackoverflow.com/questions/19966519/argparse-optional-stdin-argument
+class StreamType(argparse.Action):
+    def __call__(self, parser, namespace, values, option_string=None):
+        try:
+            setattr(namespace, self.dest, values.readline().strip())
+        except AttributeError:
+            setattr(namespace, self.dest, values)
+
+parser = argparse.ArgumentParser(description='Fetch the sequence from NCBI for the given accession number')
+parser.add_argument("accession", nargs='?', default=sys.stdin, action=StreamType)
+
+args = parser.parse_args()
 
 def fetchContig(accession):
     r = None;
@@ -10,5 +24,5 @@ def fetchContig(accession):
         r = requests.get("http://www.ncbi.nlm.nih.gov/sviewer/viewer.cgi?sendto=on&dopt=fasta&val=%s" % accession);
     return r.text.strip();
 
-print(fetchContig(sys.argv[1]));
+print(fetchContig(args.accession));
 
