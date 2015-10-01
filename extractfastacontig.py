c901bc22fc2eab20031d95f0f68a8a060f579b75
Julian Hartline <julianh2o@gmail.com>
2015-09-30 17:59:17 -1000

    changed the way extract fasta contig works

--- extractfastacontig.py
+++ extractfastacontig.py
@@ -1,14 +1,21 @@
 #!/usr/local/bin/python3
 import sys
+import argparse
+
+parser = argparse.ArgumentParser(description='Extracts a sequence from a fasta file by comparing to the header')
+parser.add_argument('fasta', type=argparse.FileType('r'), nargs="?", default=sys.stdin, help='the FASTA database')
+
+args = parser.parse_args()
 
 i = 0;
 accum = None;
-for line in sys.stdin:
+header = sys.stdin.read().strip();
+for line in args.fasta:
     i+=1;
     if (line.startswith(">")):
         if (accum is not None):
             break;
-        if (sys.argv[1] in line):
+        if (header in line):
             accum = "";
     if (accum is not None):
         accum += line;
