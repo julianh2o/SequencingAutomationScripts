95b7dcd68b0317b3f7a1deb769770c1d95b4402f
Julian Hartline <julianh2o@gmail.com>
2017-03-20 12:40:22 -0800

    improving speed, adding execution note

--- diffblast.sh
+++ diffblast.sh
@@ -29,16 +29,39 @@ mkdir -p $OUTPUT_FOLDER
 mkdir -p $MAFFT_OUTPUT_FOLDER
 mkdir -p $MAFFT_RTF_OUTPUT_FOLDER
 
-cat NEW_ACCESSIONS.txt | while read ACC; do
-    [ -z "$ACC" ] && continue #skip blank lines
+DATE=`date +%Y-%m-%d`
+
+echo "//[$DATE] Running diffblast: db:$DB entrez:$ENTREZ fasta:$INPUT_FASTA previous:$PREVIOUS ecut:$ECUTOFF output:$OUTPUT_FOLDER,$MAFFT_OUTPUT_FOLDER,$MAFFT_RTF_OUTPUT_FOLDER" >> $PREVIOUS
+
+viewblast.py list INITIAL_BLAST.blast -f '{Hit_accession}|{Hit_num}|{e}|{Hit_def}|{Hit_hsps/Hsp/Hsp_bit-score}|{Hit_hsps/Hsp/Hsp_identity}|{Hit_hsps/Hsp/Hsp_align-len}|{Hit_hsps/Hsp/Hsp_hit-frame}|{cover}' | while read LINE; do
+    ACC=`echo $LINE | awk -F'|' '{print $1}'`
+    INDEX=`echo $LINE | awk -F'|' '{print $2}'`
+    E=`echo $LINE | awk -F'|' '{print $3}'`
+    DESCRIPTION=`echo $LINE | awk -F'|' '{print $4}'`
+    HSP_BIT_SCORE=`echo $LINE | awk -F'|' '{print $5}'`
+    HSP_IDENTITY=`echo $LINE | awk -F'|' '{print $6}'`
+    HSP_ALIGN_LEN=`echo $LINE | awk -F'|' '{print $7}'`
+    HSP_HIT_FRAME=`echo $LINE | awk -F'|' '{print $8}'`
+    COVER=`echo $LINE | awk -F'|' '{print $9}'`
+    ISNEW=`cat NEW_ACCESSIONS.txt | grep $ACC`
+
+#echo "ACC: $ACC"
+#echo "index: $INDEX"
+#echo "e: $E"
+#echo "desc: $DESCRIPTION"
+#echo "bit score: $HSP_BIT_SCORE"
+#echo "ident: $HSP_IDENTITY"
+#echo "align len: $HSP_ALIGN_LEN"
+#echo "hit frame: $HSP_HIT_FRAME"
+#echo "cover: $COVER"
+
+    [ -z $ISNEW ] && continue
 
     FASTA_NAME=$ACC
     FASTA_PATH=$OUTPUT_FOLDER/$FASTA_NAME.txt
     MAFFT_FASTA_PATH=$MAFFT_OUTPUT_FOLDER/$FASTA_NAME.fasta
     MAFFT_RTF_PATH=$MAFFT_RTF_OUTPUT_FOLDER/$FASTA_NAME.rtf
 
-    INDEX=`viewblast.py list INITIAL_BLAST.blast -f '{Hit_accession} {Hit_num}' | grep $ACC | awk '{print $2}'`
-    E=`viewblast.py info $INDEX INITIAL_BLAST.blast -f '{e}'`
     TOOWEAK=`awk 'BEGIN { print ('$E'>'$ECUTOFF')? "1" : "0" }'`
 
     if [[ "$TOOWEAK" = "1" ]]; then
@@ -48,21 +71,20 @@ cat NEW_ACCESSIONS.txt | while read ACC; do
 
     if [ ! -f $FASTA_PATH ]; then
         echo "Downloading into $FASTA_NAME (e: $E)";
-        HEADER=`viewblast.py info $INDEX INITIAL_BLAST.blast -f '{Hit_accession} {Hit_def} {Hit_hsps/Hsp/Hsp_bit-score} {e} {Hit_hsps/Hsp/Hsp_identity} {Hit_hsps/Hsp/Hsp_align-len}'`
-        FRAME=`viewblast.py info $INDEX INITIAL_BLAST.blast -f '{Hit_hsps/Hsp/Hsp_hit-frame}'`
-        COMMENT=`viewblast.py info $INDEX INITIAL_BLAST.blast -f '{Hit_def} e:{e} cover:{cover}'`
+        HEADER="$ACC $DESCRIPTION $HSP_HIT_SCORE $E $HSP_IDENTITY $HSP_ALIGN_LEN"
         NOHEADER=`fetchaccession.py $ACC | fastafromtraces.sh | tail -n -1`
-        echo -e ">$HEADER\n$NOHEADER" | translate.py -r $FRAME > $FASTA_PATH
+
+        echo -e ">$HEADER\n$NOHEADER" | translate.py -r $HSP_HIT_FRAME > $FASTA_PATH
 
         MAFFTFASTA=`cat $INPUT_FASTA $FASTA_PATH`
         echo "$MAFFTFASTA" > TMP_MAFFT_INPUT.fasta
         mafft TMP_MAFFT_INPUT.fasta > $MAFFT_FASTA_PATH 2> /dev/null
+
         ANALYSIS=`alignmentanalysis.py $MAFFT_FASTA_PATH`
         cat $MAFFT_FASTA_PATH | formatmafft.py --header "$ANALYSIS" -o $MAFFT_RTF_PATH
 
         IDENT=`echo -n "$ANALYSIS" | tail -n -1 | awk -F'\t' '{printf("%d%%",100*($4/$3))}'`
-
-        DATE=`date +%Y-%m-%d`
-        echo "$ACC //$DATE $COMMENT ident=$IDENT" >> $PREVIOUS
+        COMMENT="[$DATE] $DESCRIPTION e:$E cover:$COVER ident:$IDENT"
+        echo "$ACC //$COMMENT" >> $PREVIOUS
     fi
 done
