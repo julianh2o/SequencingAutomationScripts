148332325a4aebf6e0d370a99b5bb55e09513d1b
Julian Hartline <julianh2o@gmail.com>
2017-03-20 23:20:16 -0800

    fixing bug

--- viewblast.py
+++ viewblast.py
@@ -148,7 +148,7 @@ def calculateCoverage(hsps):
 def info(args):
     results = args.blast.read();
 
-    if (len(results.strip()) == 0) exit(1);
+    if (len(results.strip()) == 0): exit(1);
     rootNode = ET.fromstring(results);
     queryLength = int(rootNode.find("BlastOutput_iterations/Iteration/Iteration_query-len").text);
     alignments = rootNode.findall("BlastOutput_iterations/Iteration/Iteration_hits/Hit")
@@ -173,7 +173,7 @@ info_parser.set_defaults(func=info);
 def contig(args):
     results = args.blast.read();
 
-    if (len(results.strip()) == 0) exit(1);
+    if (len(results.strip()) == 0): exit(1);
     rootNode = ET.fromstring(results);
     alignments = rootNode.findall("BlastOutput_iterations/Iteration/Iteration_hits/Hit")
     if (len(alignments) == 0):
@@ -194,7 +194,7 @@ contig_parser.set_defaults(func=contig);
 def list(args):
     results = args.blast.read();
 
-    if (len(results.strip()) == 0) exit(1);
+    if (len(results.strip()) == 0): exit(1);
     rootNode = ET.fromstring(results);
     queryLength = int(rootNode.find("BlastOutput_iterations/Iteration/Iteration_query-len").text);
     alignments = rootNode.findall("BlastOutput_iterations/Iteration/Iteration_hits/Hit")
